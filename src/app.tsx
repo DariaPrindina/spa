a1534f8e6608876db729e0678e1706f8bf4b95bd
DariaPrindina <megadaria2015@yandex.ru>
2024-12-27 02:25:52 +0300

    fix router double

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter  as Router, Routes, Route, Navigate  } from 'react-router-dom';
+import { Routes, Route, Navigate  } from 'react-router-dom';
 import Products from './pages/products/Products';
 import ProductDetails from './pages/product-details/ProductDetails';
 import CreateProduct from './pages/create-product/CreateProduct';
@@ -8,18 +8,16 @@ import './app.css'
 
 const App: React.FC = () => {
   return (
-    <Router> 
-      <div className='background'>
-        <div className="circle"></div>
-        <Routes>
-          <Route path="/" element={<Navigate to="/products" />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
-          <Route path="/products/:id/edit" element={<EditProduct />} />
-          <Route path="/create" element={<CreateProduct />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className='background'>
+      <div className="circle"></div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/products/:id/edit" element={<EditProduct />} />
+        <Route path="/create" element={<CreateProduct />} />
+      </Routes>
+    </div>
   );
 };
 
